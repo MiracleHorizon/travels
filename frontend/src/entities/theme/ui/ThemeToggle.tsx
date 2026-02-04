12ff867a0f506780af4543ed521f2794ffd9da59
MiracleHorizon <alexeenkodenisjke@gmail.com>
2026-02-04 03:54:59 +0300

    feat: theme toggle group

--- frontend/src/entities/theme/ui/ThemeToggle.tsx
+++ frontend/src/entities/theme/ui/ThemeToggle.tsx
@@ -1,57 +1,48 @@
 import { Moon, Sun, Monitor } from 'lucide-react'
-import {
-  Button,
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger
-} from '@/shared/ui'
+import { ToggleGroup, ToggleGroupItem } from '@/shared/ui'
 import { useTheme } from '../model/useTheme'
 import { Theme } from '../model/types'
 
-interface ThemeToggleProps {
-  compact?: boolean
-}
-
 const themeItems = [
   {
-    title: 'Светлая',
     value: 'light',
-    Icon: Sun
+    Icon: Sun,
+    label: 'Светлая'
   },
   {
-    title: 'Темная',
     value: 'dark',
-    Icon: Moon
+    Icon: Moon,
+    label: 'Темная'
   },
   {
-    title: 'Системная',
     value: 'system',
-    Icon: Monitor
+    Icon: Monitor,
+    label: 'Системная'
   }
 ] as const
 
-export const ThemeToggle = ({ compact = false }: ThemeToggleProps) => {
-  const { changeTheme } = useTheme()
+export const ThemeToggle = () => {
+  const { theme, changeTheme } = useTheme()
+
+  const handleChangeTheme = (value: Theme) => {
+    if (value) {
+      changeTheme(value as Theme)
+    }
+  }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant='outline' size='sm'>
-          <Sun className='relative h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 dark:absolute' />
-          <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark:relative' />
-          {!compact && 'Переключить тему'}
-        </Button>
-      </DropdownMenuTrigger>
-
-      <DropdownMenuContent align='end'>
-        {themeItems.map(({ value, Icon, title }) => (
-          <DropdownMenuItem key={value} onClick={() => changeTheme(value as Theme)}>
-            <Icon className='mr-2 h-4 w-4' />
-            <span>{title}</span>
-          </DropdownMenuItem>
-        ))}
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <ToggleGroup
+      size='sm'
+      type='single'
+      variant='outline'
+      value={theme}
+      onValueChange={handleChangeTheme}
+    >
+      {themeItems.map(({ value, label, Icon }) => (
+        <ToggleGroupItem key={value} value={value} aria-label={label}>
+          <Icon />
+        </ToggleGroupItem>
+      ))}
+    </ToggleGroup>
   )
 }

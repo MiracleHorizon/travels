2293ad244e48a4b2f495c801a5f71b07750cbe03
MiracleHorizon <alexeenkodenisjke@gmail.com>
2026-02-22 23:31:08 +0300

    fix

--- backend/src/domains/geo/consts.ts
+++ backend/src/domains/geo/consts.ts
@@ -3,7 +3,6 @@ import type { Locale } from '../user-settings'
 export const YANDEX_GEOCODER_API_KEY = process.env.YANDEX_GEOCODER_API_KEY as string
 export const YANDEX_GEOCODER_API_URL = 'https://geocode-maps.yandex.ru/v1/'
 
-// Документация: https://yandex.ru/dev/maps/geocoder/doc/desc/concepts/language-docpage
 const LOCALE_TO_YANDEX_LANG: Record<Locale, string> = {
   ru: 'ru_RU',
   en: 'en_US'
